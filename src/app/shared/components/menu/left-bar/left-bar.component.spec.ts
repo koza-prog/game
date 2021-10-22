9af2545834eb189e7199a6aa55fa4d103206228b
kozaalfa <kgachu@o2.pl>
2021-10-22 12:13:19 +0200

    11

--- src/app/shared/components/menu/left-bar/left-bar.component.spec.ts
+++ src/app/shared/components/menu/left-bar/left-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LeftBarComponent } from './left-bar.component';
 
@@ -6,7 +6,7 @@ describe('LeftBarComponent', () => {
   let component: LeftBarComponent;
   let fixture: ComponentFixture<LeftBarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LeftBarComponent ]
     })
--- src/app/shared/components/menu/right-bar/right-bar.component.spec.ts
+++ src/app/shared/components/menu/right-bar/right-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RightBarComponent } from './right-bar.component';
 
@@ -6,7 +6,7 @@ describe('RightBarComponent', () => {
   let component: RightBarComponent;
   let fixture: ComponentFixture<RightBarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RightBarComponent ]
     })
